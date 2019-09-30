6a96ad9f22c436854eed160503d87a55cd066113
joshuamsmith413 <joshuamsmith413@gmail.com>
2019-09-30 12:28:35 -0400

    added jwt for auth

--- src/containers/AuthContainer.js
+++ src/containers/AuthContainer.js
@@ -5,44 +5,11 @@ import Login from '../components/Login.js';
 
 class AuthContainer extends React.Component {
 
-  state = {
-    fields: {
-      name: "",
-      password: ""
-    },
-    error: ""
-  }
-
-
-  handleChange = e => {
-    const userInfo = { ...this.state.fields, [e.target.name]: e.target.value };
-    this.setState({ fields: userInfo });
-  }
-
- handleSignUp = e => {
-   e.preventDefault()
-   fetch('http://localhost:3000/users', {
-     method: "POST",
-     headers: {
-       'Content-Type': 'application/json',
-       Accept: 'application/json'
-     },
-     body: JSON.stringify(this.state.fields)
-   })
-   .then(r => r.json())
-   .then(data => {
-      data.error ? alert(`${data.error}`) : alert(`Welcome ${this.state.fields.name}`)
-    })
-    .then(this.props.history.push("/"))
- }
-
-
-
   render() {
     return(
       <div id="AuthContainer">
-        <SignUp handleChange={this.handleChange} fields={this.state.fields} handleSignUp={this.handleSignUp}/>
-        <Login />
+        <SignUp />
+        <Login handleLogin={this.props.handleLogin} handleChange={this.props.handleChange} newUser={this.props.newUser}/>
       </div>
     )
   }
